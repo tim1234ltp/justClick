896e5ac2bd907dd344d44a36711c0107cfbef954
tim1234ltp <tim1234.ltp@gmail.com>
2018-04-22 19:45:41 +0800

    I am done.

---
+++ JustClick/GameWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JustClick
+{
+    /// <summary>
+    /// GameWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int clickNum = 0;
+
+        private void exitBtn(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clickNum++;
+            score.Content = clickNum.ToString();
+        }
+    }
+}
